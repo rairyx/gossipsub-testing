1d6f3d1bb8a5370511868b78c00a16bc9fcc9502
Jason Tran <jasonatran@gmail.com>
2020-01-11 13:30:13 -0800

    plot cumulative message times

--- analysis/plot_analysis.py
+++ analysis/plot_analysis.py
@@ -1,14 +1,12 @@
 import glob
 import json
 import sys
-
+import numpy as np
+import matplotlib.pyplot as plt
 # change this to your virtualenv path
 sys.path.append('../../venv/gs-test/lib/python3.6/site-packages')
 
 # %matplotlib inline
-
-import numpy as np
-import matplotlib.pyplot as plt
 
 metricProps = {
     'messageID': '',
@@ -18,26 +16,26 @@ metricProps = {
     'relativeMessageRedundancy': ''
 }
 
+
 class Metric(object):
-    def __init__(self, props = metricProps):        
+    def __init__(self, props=metricProps):
         self.messageID = props['messageID']
         self.originatorHostID = props['originatorHostID']
         self.totalNanoTime = int(props['totalNanoTime'])
         self.lastDeliveryHop = float(props['lastDeliveryHop'])
-        self.relativeMessageRedundancy = float(props['relativeMessageRedundancy'])
+        self.relativeMessageRedundancy = \
+            float(props['relativeMessageRedundancy'])
 
-def computeMetrics(algorithm = 'gossip', filename = None, fig = 0):
+def computeMetrics(algorithm='gossip', filename=None, fig=0):
     metrics = []
     nanoTimes = []
     ldh = []
     rmr = []
-    
-    if filename == None:
+
+    if filename is None:
         pubsubAnalysisFiles = glob.glob('*.json')
     else:
         pubsubAnalysisFiles = glob.glob('{}'.format(filename))
-        
-
 
     for pubsubAnalysisFile in pubsubAnalysisFiles:
         tmpMetrics = []
@@ -47,7 +45,7 @@ def computeMetrics(algorithm = 'gossip', filename = None, fig = 0):
             for metric in data:
                 m = Metric(metric)
                 tmpMetrics.append(m)
-                
+
                 nanoTimes.append(m.totalNanoTime)
                 ldh.append(m.lastDeliveryHop)
                 rmr.append(m.relativeMessageRedundancy)
@@ -56,12 +54,19 @@ def computeMetrics(algorithm = 'gossip', filename = None, fig = 0):
 
     plt.figure(fig)
 
-    _ = plt.hist(nanoTimes, bins='auto', histtype='step')
-    plt.title("{} - Total Nano Time Histogram".format(algorithm))
-    plt.xlabel('Total Nano Time')
-    plt.ylabel('Occurrence')
+    nanoTimesArr = np.asarray(nanoTimes)
+    # a, b, c = plt.hist(nanoTimes, bins='auto', histtype='step',
+    #                    cumulative=True)
+    counts, bin_edges = np.histogram(nanoTimesArr, bins=2000, density=True)
+    cdf = np.cumsum(counts)
+    plt.plot(bin_edges[1:], cdf/cdf[-1])
 
-    
+
+    print(np.percentile(nanoTimesArr, 90))
+    plt.title("{} - Total Nano Time Histogram".format(algorithm))
+    plt.xlabel('Dissemination Time (ns)')
+    plt.ylabel('Percent of Messages')
+
     nanoMean = np.mean(nanoTimes)
     nanoMedian = np.median(nanoTimes)
     nanoStd = np.std(nanoTimes)
@@ -79,13 +84,14 @@ def computeMetrics(algorithm = 'gossip', filename = None, fig = 0):
     print('Last Delivery Hop - mean: {}, median: {}, std: {}'.format(ldhMean, ldhMedian, ldhStd))
     print('Relative Message Redundancy - mean: {}, median: {}, std: {}'.format(rmrMean, rmrMedian, rmrStd))
 
+
 num = 0
-for file in glob.glob('analysis*.json'):
-    try: 
+for file in glob.glob('final_json/analysis2b.json'):
+    try:
         print("\n\nfile:" + file)
         computeMetrics('gossip', file, num)
     except KeyError:
-        #not sure why this is happening
+        # not sure why this is happening
         print("relativeMessageRedundancy key error, skipping analysis")
     num += 1
 
